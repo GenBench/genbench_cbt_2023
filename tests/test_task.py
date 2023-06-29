1dc902239492ba5df48684a1ec05a1055d382e4b
Amirhossein Kazemnejad <2122102+kazemnejad@users.noreply.github.com>
2023-06-29 19:26:43 -0400

    Add more unit testsˆ

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,9 +1,10 @@
+import copy
 import re
 
 import pytest
 from datasets import IterableDataset
 
-from genbench.api import DatasetSplit, TaskType, PreparationStrategy
+from genbench.api import DatasetSplit, TaskType, PreparationStrategy, EvaluationResult
 from genbench.task import Task
 from genbench.utils.file import load_jsonnet
 from genbench.utils.tasks import get_task_dir
@@ -138,15 +139,60 @@ def test_no_duplicate_examples(task_obj):
             assert item not in hash_set
             hash_set.add(item)
 
+    if task_obj.config.preparation_strategies.prompt_based_testing is not None:
+        test_set = task_obj.get_prepared_datasets(
+            preparation_strategy=PreparationStrategy.PROMPT_BASED_TESTING, shot_list=[0]
+        )[0]
 
-def test_no_iterable_dataset(task_obj):
+        hash_set = set()
+        for example in test_set:
+            item = (example["input"], example["target"])
+            assert item not in hash_set
+            hash_set.add(item)
+
+
+def test_no_iterable_dataset(task_obj: Task):
     """Test case to verify if the data source does not contain IterableDataset"""
     if (
         "get_prepared_datasets" in task_obj.__class__.__dict__
         or "get_datasets_raw" in task_obj.__class__.__dict__
     ):
         pytest.skip("Task has a custom dataset builder.")
+        return
 
     data_source = task_obj._load_data_source()
     for split in data_source:
         assert not isinstance(data_source[split], IterableDataset)
+
+
+def test_perfect_score_for_gold_instances(task_obj: Task):
+    """Test case to verify if the task has perfect score for gold instances"""
+    if "evaluate_predictions" in task_obj.__class__.__dict__:
+        pytest.skip("Task has a custom evaluate_predictions method.")
+        return
+
+    if task_obj.config.preparation_strategies.finetuning is not None:
+        test_set = task_obj.get_prepared_datasets(
+            preparation_strategy=PreparationStrategy.FINETUNING
+        )[DatasetSplit.TEST.value]
+    elif task_obj.config.preparation_strategies.prompt_based_testing is not None:
+        test_set = task_obj.get_prepared_datasets(
+            preparation_strategy=PreparationStrategy.PROMPT_BASED_TESTING, shot_list=[0]
+        )[0]
+    else:
+        pytest.skip("Task has no prepared datasets.")
+        return
+
+    predictions = [{"target": copy.deepcopy(example["target"])} for example in test_set]
+    result: EvaluationResult = task_obj.evaluate_predictions(
+        predictions=predictions, gold=test_set
+    )
+    for metric_config in task_obj.config.evaluation_metrics:
+        best_score = metric_config.best_score
+        hf_id = metric_config.hf_id
+        if isinstance(hf_id, str):
+            hf_id = [hf_id]
+        metric_id = "_".join(hf_id)
+        keys = [k for k in result if k.startswith(f"hf_{metric_id}__")]
+        for key in keys:
+            assert result[key] == best_score
