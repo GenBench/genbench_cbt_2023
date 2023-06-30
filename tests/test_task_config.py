b3cf98da6d352f41a0dfa4db4d5105c20229e737
Amirhossein Kazemnejad <2122102+kazemnejad@users.noreply.github.com>
2023-06-30 11:54:48 -0400

    Fix style

--- tests/test_task_config.py
+++ tests/test_task_config.py
@@ -36,7 +36,7 @@ def default_jsonnet_config() -> str:
     }"""
 
     evaluation_metrics = """[
-        {            
+        {
             hf_id: 'accuracy',
             git_commit_sha: "758135da6a37ce962b7bc38c6dd5eab672d2b742",
             best_score: 1.0,
