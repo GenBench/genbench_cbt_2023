aaac8f243b2d8b8ac8ad405a9121adae5f3d549a
Amirhossein Kazemnejad <2122102+kazemnejad@users.noreply.github.com>
2023-07-01 14:41:33 -0400

    Fix style

--- src/genbench/tasks/my_awesome_task_dict/subtask_1/task.py
+++ src/genbench/tasks/my_awesome_task_dict/subtask_1/task.py
@@ -4,6 +4,6 @@ from genbench import Task
 class MyAwesomeTaskDictSubtask1(Task):
     def format_example(self, example):
         return {
-        	"input": f"{example['input']} hello inn11!",
-        	"target": f"{example['target']} hello out11!",
+            "input": f"{example['input']} hello inn11!",
+            "target": f"{example['target']} hello out11!",
         }
--- src/genbench/tasks/my_awesome_task_dict/subtask_2/task.py
+++ src/genbench/tasks/my_awesome_task_dict/subtask_2/task.py
@@ -4,6 +4,6 @@ from genbench import Task
 class MyAwesomeTaskDictSubtask2(Task):
     def format_example(self, example):
         return {
-        	"input": f"{example['input']} hello inn222!",
-        	"target": f"{example['target']} hello out222!",
+            "input": f"{example['input']} hello inn222!",
+            "target": f"{example['target']} hello out222!",
         }
