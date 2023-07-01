704ca439e99a7323b1d01498de3b59ca675cebe1
Amirhossein Kazemnejad <2122102+kazemnejad@users.noreply.github.com>
2023-07-01 12:59:16 -0400

    Add My Awesome Task

--- src/genbench/tasks/my_awesome_task/task.py
+++ src/genbench/tasks/my_awesome_task/task.py
@@ -4,6 +4,6 @@ from genbench import Task
 class MyAwesomeTaskTask(Task):
     def format_example(self, example):
         return {
-        	"input": f"{example['input']} hello in!",
+        	"input": f"{example['input']} hello inn!",
         	"target": f"{example['target']} hello out!",
         }
