16b1cf7bf0e198896f8d7b3318eb4f59d4d6f6d4
Betswish <j.qi@rug.nl>
2023-07-26 16:32:36 +0200

    Fix

--- src/genbench/tasks/cross_lingual_consistency/task.py
+++ src/genbench/tasks/cross_lingual_consistency/task.py
@@ -1,9 +1,11 @@
-from genbench import Task
-import json
 import argparse
-import numpy as np
+import json
 from typing import Any, Dict, List
+
 import datasets
+import numpy as np
+
+from genbench import Task
 
 
 class CrossLingualConsistencyTask(Task):
@@ -16,17 +18,16 @@ class CrossLingualConsistencyTask(Task):
         pass
 
     def format_example(self, example: Dict[str, Any]) -> Dict[str, Any]:
-        lang1_rankings = eval(example['lang1_rankings'])
-        lang2_rankings = eval(example['lang2_rankings'])
-        cand1 = eval(example['lang1_cands'])
-        cand2 = eval(example['lang2_cands'])
+        lang1_rankings = eval(example["lang1_rankings"])
+        lang2_rankings = eval(example["lang2_rankings"])
+        cand1 = eval(example["lang1_cands"])
+        cand2 = eval(example["lang2_cands"])
 
         def softmax(x):
-            #Compute softmax values for each sets of scores in x.
+            # Compute softmax values for each sets of scores in x.
             return np.exp(x) / np.sum(np.exp(x), axis=0)
 
-        weight_metric = 'softmax' # softmax norm1 norm2
-
+        weight_metric = "softmax"  # softmax norm1 norm2
 
         cand_list1 = [[j for j in list(i.keys())] for i in cand1]
         cand_list2 = [[j for j in list(i.keys())] for i in cand2]
@@ -40,26 +41,27 @@ class CrossLingualConsistencyTask(Task):
             candidate1 = cand_list1[i]
             candidate2 = cand_list2[i]
 
-            order = [len(ranking1)-i for i in range(len(ranking1))]
+            order = [len(ranking1) - i for i in range(len(ranking1))]
             order = np.array(order)
 
-            if weight_metric == 'softmax':
+            if weight_metric == "softmax":
                 weight = softmax(order)
-            elif weight_metric == 'norm1':
-                weight = order/np.sum(order)
+            elif weight_metric == "norm1":
+                weight = order / np.sum(order)
             else:
-                weight = np.power(order, 2)/np.sum(np.power(order, 2))
+                weight = np.power(order, 2) / np.sum(np.power(order, 2))
 
             for j in range(len(ranking1)):
-                set1 = {candidate1.index(i) for i in ranking1[:j+1]}
-                set2 = {candidate2.index(i) for i in ranking2[:j+1]}
+                set1 = {candidate1.index(i) for i in ranking1[: j + 1]}
+                set2 = {candidate2.index(i) for i in ranking2[: j + 1]}
 
                 cover = set1.intersection(set2)
-                num_consistent += weight[j] * (len(cover)/len(set1))
+                num_consistent += weight[j] * (len(cover) / len(set1))
 
         return {
-                "rankings of candidates when probing Bloom-3b with en factual queries in BMLAMA:": str(lang1_rankings),
-                "rankings of candidates when probing Bloom-3b with es factual queries in BMLAMA:": str(lang2_rankings),
-                "(Computed with our proposed RankC metric) Cross Lingual Consistency between en and es factual knowledge in Bloom-3b": str(num_consistent/len(lang1_rankings)),
-                }
-
+            "rankings of candidates when probing Bloom-3b with en factual queries in BMLAMA:": str(lang1_rankings),
+            "rankings of candidates when probing Bloom-3b with es factual queries in BMLAMA:": str(lang2_rankings),
+            "(Computed with our proposed RankC metric) Cross Lingual Consistency between en and es factual knowledge in Bloom-3b": str(
+                num_consistent / len(lang1_rankings)
+            ),
+        }
