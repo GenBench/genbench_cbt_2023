31b09af92830bc2e1f3b0ffeae2aec707e548c23
LucWeber <lucasweber000@gmail.com>
2023-07-28 10:46:55 +0200

    Style and quality checks

---
+++ src/genbench/tasks/icl_consistency_test/example_evaluation.py
@@ -0,0 +1,118 @@
+"""
+EXAMPLE USAGE OF ICL CONSISTENCY TEST
+
+This script requires additional packages to be installed:
+
+pip install torch
+pip install git+https://github.com/huggingface/transformers.git
+pip install bitsandbytes
+pip install accelerate
+
+"""
+import string
+from typing import Dict, List
+
+import torch
+import transformers
+from torch import Tensor
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from genbench import load_task
+from genbench.api import PreparationStrategy
+
+
+N_DATAPOINTS = 50
+MODEL_NAME = "huggyllama/llama-7b"
+BATCH_SIZE = 8
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class Generator:
+    def __init__(self, model_name="huggyllama/llama-7b"):
+        self.max_new_tokens = 4  # some labels consist of up to 4 tokens
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            model_name,
+            device_map="auto",
+            padding_side="left",
+        )
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.model = transformers.AutoModelForCausalLM.from_pretrained(
+            model_name,
+            load_in_8bit=True,
+            device_map="auto",
+        ).eval()
+
+        self.generation_config = transformers.GenerationConfig(
+            do_sample=False,
+            return_dict_in_generate=False,
+            output_scores=True,
+            max_new_tokens=self.max_new_tokens,
+            return_full_text=False,
+        )
+
+    def generate(self, prompt) -> List[str]:
+        inputs = self.tokenizer(prompt, return_tensors="pt", padding=True, truncation=True)
+
+        input_ids = inputs["input_ids"].to(device)
+        attention_mask = inputs["attention_mask"].to(device)
+
+        generation_output = self.model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            generation_config=self.generation_config,
+        )
+
+        outputs = self.tokenizer.batch_decode(generation_output[:, input_ids.shape[1] :])
+
+        # do some post-processing
+        outputs = [o.strip().split()[0].translate(str.maketrans("", "", string.punctuation)) for o in outputs]
+
+        return outputs
+
+    def make_predictions(self, dataset, bs=8) -> Dict[str, Dict[str, str]]:
+        out = {}
+        dl = DataLoader(dataset=dataset, batch_size=bs, num_workers=0)
+
+        with torch.no_grad():
+            for batch in tqdm(dl):
+                prediction = self.generate(batch["input"])
+
+                # organize predictions into output dictionary
+                for i, (data_ID, setup_ID) in enumerate(zip(batch["data_ID"], batch["setup_ID"])):
+                    data_ID = str(data_ID.item() if isinstance(data_ID, Tensor) else data_ID)
+                    if setup_ID in out.keys():
+                        out[setup_ID].update({data_ID: prediction[i]})
+                    else:
+                        out[setup_ID] = {data_ID: prediction[i]}
+
+        return out
+
+
+if __name__ == "__main__":
+    # Load the task
+    task = load_task("icl_consistency_test")
+    ds = task.get_prepared_datasets(PreparationStrategy.PROMPT_BASED_TESTING, shot_list=[0])[0]
+
+    # Selecting a subset of example for illustration purposes
+    subset = list(set(ds["data_ID"]))[:N_DATAPOINTS]
+    ds = ds.filter(lambda x: x["data_ID"] in subset)
+
+    # Generate predictions for the dataset
+    generator = Generator(model_name=MODEL_NAME)
+    predictions = generator.make_predictions(ds, bs=BATCH_SIZE)
+
+    # OPTIONAL: The ICL-consistency test provides the option to add factors to the analysis by using the
+    # `add_factor` method.
+    add_external_factor = False
+    if add_external_factor:
+        predictions_external_factor = ...  # some function generating alternative predictions
+        predictions = task.add_factor(data=(predictions, predictions_external_factor), factor="<my-external-factor>")
+
+    # Evaluate the predictions
+    results = task.evaluate_predictions(predictions=predictions, gold=ds)
+
+    print("EVALUATED SUCCESSFULLY!")
+    print(f'Exact-match accuracies: \n{results["exact_match_accuracy"]["accuracy"]}')
+    print(f'Consistency: \n{results["kappas"]}')
--- src/genbench/tasks/icl_consistency_test/task.py
+++ src/genbench/tasks/icl_consistency_test/task.py
@@ -1,27 +1,30 @@
-from typing import Any, Dict, List, Tuple
-from genbench import Task
-
-from sklearn.metrics import cohen_kappa_score
-from pandas import DataFrame
+from typing import Any, Dict, Tuple
 
 import datasets
+from pandas import DataFrame
+from sklearn.metrics import cohen_kappa_score
 
-LABELS = [['Correct', 'True', 'Always', 'Yes', 'Guaranteed', 'Duplicates'],  # `correct` labels
-          ['Inconclusive', 'Possible', 'Sometimes', 'Maybe', 'Neither'],  # `neutral` labels
-          ['Impossible', 'Never', 'Incorrect', 'False', 'No', 'Not Duplicates'], ]  # `incorrect` labels
+from genbench import Task
+
+
+LABELS = [
+    ["Correct", "True", "Always", "Yes", "Guaranteed", "Duplicates"],  # `correct` labels
+    ["Inconclusive", "Possible", "Sometimes", "Maybe", "Neither"],  # `neutral` labels
+    ["Impossible", "Never", "Incorrect", "False", "No", "Not Duplicates"],  # `incorrect` labels
+]
 
 LABEL_TO_NUMERIC = {}
 LABEL_TO_NUMERIC.update(dict([(label, i) for i, label_subset in enumerate(LABELS) for label in label_subset]))
 LABEL_TO_NUMERIC.update(dict([(label.lower(), i) for i, label_subset in enumerate(LABELS) for label in label_subset]))
 
 factors = [
-    'balanced_labels',
-    'one_label',
-    'cross_task',
-    'cross_instructions',
-    'n_shots',
-    'instructions',
-    'instruction_quality',
+    "balanced_labels",
+    "one_label",
+    "cross_task",
+    "cross_instructions",
+    "n_shots",
+    "instructions",
+    "instruction_quality",
 ]
 
 
@@ -29,10 +32,11 @@ class IclConsistencyTestTask(Task):
     """Python implementation of the ICL consistency test task."""
 
     def evaluate_predictions(
-            self,
-            *,
-            predictions: Dict[str, Dict[str, Any]],
-            gold: datasets.Dataset,
+        self,
+        *,
+        predictions: Dict[str, Dict[str, Any]],
+        gold: datasets.Dataset,
+        save_path: str = None,
     ) -> Dict[str, Any]:
         """Evaluate the predictions of the model against the gold data.
         Calculating exact match accuracy plus consistency across all setups (Cohen's kappa).
@@ -51,40 +55,37 @@ class IclConsistencyTestTask(Task):
         self._set_factors()
 
         gold_pandas = gold.to_pandas()
-        gold_pandas['data_ID'] = gold_pandas['data_ID'].astype(str)
-        gold_labels_numeric = gold_pandas.set_index('data_ID')['target_numeric'].to_dict()
+        gold_pandas["data_ID"] = gold_pandas["data_ID"].astype(str)
+        gold_labels_numeric = gold_pandas.set_index("data_ID")["target_numeric"].to_dict()
 
         results_df = self._create_df(predictions, gold_labels_numeric)
-        results_df = results_df.sort_values(by=['setup_ID', 'data_ID'])
+        results_df = results_df.sort_values(by=["setup_ID", "data_ID"])
         self._assert_equal_data_ids(results_df)
 
         # Compute the exact match accuracy for each setup.
-        em = {factor: [] for factor in self.factors + ['accuracy']}
-        for setup_ID, setup_predictions in results_df.groupby('setup_ID'):
+        em = {factor: [] for factor in self.factors + ["accuracy"]}
+        for setup_ID, setup_predictions in results_df.groupby("setup_ID"):
             temp = self._convert_numeric_id_to_dict(setup_ID, n_repetitions=1)
             for factor in self.factors:
                 em[factor].extend(temp[factor])
-            em['accuracy'].append(
-                (setup_predictions['predictions_numeric'] == setup_predictions['target_numeric']).mean())
+            em["accuracy"].append(
+                (setup_predictions["predictions_numeric"] == setup_predictions["target_numeric"]).mean()
+            )
 
         # Compute the Cohen's kappa for consistency.
         kappas = {}
         for factor in self.factors:
-            factor_present = results_df.loc[results_df[factor] == '1']['predictions_numeric']
-            factor_absent = results_df.loc[results_df[factor] == '0']['predictions_numeric']
+            factor_present = results_df.loc[results_df[factor] == "1"]["predictions_numeric"]
+            factor_absent = results_df.loc[results_df[factor] == "0"]["predictions_numeric"]
 
-            # mask out predictions that are out-of-label-distribution 
+            # mask out predictions that are out-of-label-distribution
             mask = [(f1 != -1 and f2 != -1) for f1, f2 in zip(factor_absent, factor_present)]
-            try:
-                factor_present, factor_absent = factor_present[mask], factor_absent[mask]
-            except:
-                breakpoint()
+            factor_present, factor_absent = factor_present[mask], factor_absent[mask]
 
             kappas[factor] = cohen_kappa_score(factor_present, factor_absent)
 
         # Return the evaluation metrics.
-        return {"exact_match_accuracy": em,
-                "kappas": kappas}
+        return {"exact_match_accuracy": em, "kappas": kappas}
 
     def add_factor(self, data: Tuple[Dict, Dict], factor: str) -> Dict[str, Dict[str, Any]]:
         """Concatenate the data with the factor present and absent and update the setup_IDs accordingly. Also add the
@@ -102,8 +103,8 @@ class IclConsistencyTestTask(Task):
         setup_ids1 = list(data[1].keys())
 
         for setup_id0, setup_id1 in zip(setup_ids0, setup_ids1):
-            updated_id0 = setup_id0 + '0'
-            updated_id1 = setup_id1 + '1'
+            updated_id0 = setup_id0 + "0"
+            updated_id1 = setup_id1 + "1"
             data[0][updated_id0] = data[0].pop(setup_id0)
             data[1][updated_id1] = data[1].pop(setup_id1)
 
@@ -124,17 +125,16 @@ class IclConsistencyTestTask(Task):
             keep_present: whether to keep data with the factor present or absent.
         """
         self._set_factors()
-        #breakpoint()
+        # breakpoint()
         len_setup_ID_preamble = 4
         index_factor = self.factors.index(factor) + len_setup_ID_preamble
         realisation_to_keep = str(int(keep_present))
 
         # filter out all unwanted datapoints and adapt setup_IDs to exclude factor
-        data = data.filter(lambda x: x['setup_ID'][index_factor] == realisation_to_keep)
-        data = data.map(lambda x: {**x, "setup_ID": x["setup_ID"][:index_factor] + x["setup_ID"][index_factor + 1:]} )
+        data = data.filter(lambda x: x["setup_ID"][index_factor] == realisation_to_keep)
+        data = data.map(lambda x: {**x, "setup_ID": x["setup_ID"][:index_factor] + x["setup_ID"][index_factor + 1 :]})
 
         # Remove factor from list of factors.
-        
         self.factors.pop(self.factors.index(factor))
 
         return data
@@ -152,18 +152,19 @@ class IclConsistencyTestTask(Task):
         Returns:
             A pandas dataframe containing the predictions and gold data.
         """
-        additional_keys = ['predictions_numeric', 'target_numeric', 'setup_ID', 'data_ID']
+        additional_keys = ["predictions_numeric", "target_numeric", "setup_ID", "data_ID"]
         results_dict = {factor: [] for factor in self.factors + additional_keys}
 
         for setup_ID, predictions_setup in predictions.items():
             data_ids = list(predictions_setup.keys())
             n_datapoints = len(data_ids)
 
-            results_dict['data_ID'].extend(data_ids)
-            results_dict['setup_ID'].extend([setup_ID] * n_datapoints)
-            results_dict['target_numeric'].extend(gold_labels[data_id] for data_id in data_ids)
-            results_dict['predictions_numeric'].extend(self._label_to_numeric(predictions_setup[data_id]) for
-                                                       data_id in data_ids)
+            results_dict["data_ID"].extend(data_ids)
+            results_dict["setup_ID"].extend([setup_ID] * n_datapoints)
+            results_dict["target_numeric"].extend(gold_labels[data_id] for data_id in data_ids)
+            results_dict["predictions_numeric"].extend(
+                self._label_to_numeric(predictions_setup[data_id]) for data_id in data_ids
+            )
 
             temp = self._convert_numeric_id_to_dict(setup_ID, n_repetitions=n_datapoints)
             for factor in self.factors:
@@ -172,7 +173,7 @@ class IclConsistencyTestTask(Task):
         return DataFrame(results_dict)
 
     def _set_factors(self):
-        if not hasattr(self, 'factors'):
+        if not hasattr(self, "factors"):
             self.factors = factors
 
     def _convert_numeric_id_to_dict(self, setup_id: str, n_repetitions: int = 1) -> Dict[str, Any]:
@@ -184,7 +185,7 @@ class IclConsistencyTestTask(Task):
         Returns:
             A dict containing factors as keys and the factor realisation as value.
         """
-        setup_id = setup_id.split('_')[1]
+        setup_id = setup_id.split("_")[1]
 
         setup_dict = {}
         for factor, value in zip(self.factors, setup_id):
@@ -211,8 +212,8 @@ class IclConsistencyTestTask(Task):
         Args:
             results_df: A pandas dataframe containing the predictions and gold data.
         """
-        used_data_ids = results_df['data_ID'].unique()
-        for setup_ID in results_df['setup_ID'].unique():
-            assert used_data_ids.sort() == results_df.loc[results_df['setup_ID'] == setup_ID][
-                'data_ID'].unique().sort(), \
-                "Not all data_IDs are the same for all setups. Check for missing predictions!"
+        used_data_ids = results_df["data_ID"].unique()
+        for setup_ID in results_df["setup_ID"].unique():
+            assert (
+                used_data_ids.sort() == results_df.loc[results_df["setup_ID"] == setup_ID]["data_ID"].unique().sort()
+            ), "Not all data_IDs are the same for all setups. Check for missing predictions!"
