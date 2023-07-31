94d1181b12cf63e29d3145c71f4202cda5657f5a
MaikeZuefle <maike@r-zuefle.de>
2023-07-31 08:55:00 +0100

    test set

--- src/genbench/tasks/hate_speech_detection/test_stuff.py
+++ src/genbench/tasks/hate_speech_detection/test_stuff.py
@@ -3,10 +3,10 @@ from genbench.api import PreparationStrategy
 
 task = load_task("hate_speech_detection")
 ds = task.get_prepared_datasets(
-    PreparationStrategy.PROMPT_BASED_TESTING, 
-    shot_list=[0]
-)[0]
-print(ds[0])
+    PreparationStrategy.FINETUNING)
+print(ds["test"])
+
+
 # {
 #   'input': 'Add two numbers together\n\nQ: 300 + 80\nA: ', 
 #   'target': '380', 
