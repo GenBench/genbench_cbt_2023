25991d26e084eae692bd39834fcdfb3877fa89b8
Anssi Moisio <anssi.moisio@aalto.fi>
2023-08-01 16:26:49 +0300

    Fix style

--- src/genbench/tasks/dbca_deprel/task.py
+++ src/genbench/tasks/dbca_deprel/task.py
@@ -1,8 +1,11 @@
 from typing import Any, Dict, List
+
 import datasets
-from sacrebleu.metrics import BLEU
-from genbench import Task
 import torch
+from sacrebleu.metrics import BLEU
+
+from genbench import Task
+
 
 class DbcaDeprelTask(Task):
     """This task evaluates how well an NMT model generalises to a shifted distribution of
@@ -38,19 +41,17 @@ class DbcaDeprelTask(Task):
 
         return {"BLEU": bleu_result}
 
-
     def chernoff_coef(self, vec1, vec2, alpha):
         """
         The Chernoff coefficient c is a similarity measure C_{alpha}(P||Q)
-        = sum_k[p_k^alpha * q_k^(1-alpha)] e[0,1] between two (probability) 
+        = sum_k[p_k^alpha * q_k^(1-alpha)] e[0,1] between two (probability)
         distributions P and Q. The alpha parameter determines if we want to
         measure whether Q includes elements that are not in P.
         """
         if alpha < 0 or alpha > 1:
             raise ValueError("alpha must be in [0,1]")
         # use log to avoid underflow
-        return torch.sum(torch.exp((torch.log(vec1) * alpha) +
-                                (torch.log(vec2) * (1-alpha))), axis=0)
+        return torch.sum(torch.exp((torch.log(vec1) * alpha) + (torch.log(vec2) * (1 - alpha))), axis=0)
 
     def normalize_vector(self, vector):
         """Normalize a vector to have sum 1."""
@@ -62,9 +63,11 @@ class DbcaDeprelTask(Task):
         Atom divergence is 1 - Chernoff coefficient, with alpha=0.5.
         Compound divergence is 1 - Chernoff coefficient, with alpha=0.1.
         """
-        return float(1 - self.chernoff_coef(self.normalize_vector(vec1),
-                                            self.normalize_vector(vec2), alpha))
+        return float(1 - self.chernoff_coef(self.normalize_vector(vec1), self.normalize_vector(vec2), alpha))
 
-    def count_freqs(self, dataset,):
+    def count_freqs(
+        self,
+        dataset,
+    ):
         """TODO: Return the frequencies of atoms and compounds in the dataset."""
         pass
