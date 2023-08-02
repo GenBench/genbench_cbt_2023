2d491ac760766a20e53012c83c5fcef7ea4d5791
Khuyagbaatar Batsuren <khuyagbaatar@gmail.com>
2023-08-03 04:32:09 +0800

    adding get_dataset_raw function

--- src/genbench/tasks/oov_generalisation/dow/task.py
+++ src/genbench/tasks/oov_generalisation/dow/task.py
@@ -2,4 +2,19 @@ from genbench import Task
 
 
 class OovGeneralisationDow(Task):
-    pass
+    
+    def get_datasets_raw(self) -> Mapping[str, datasets.Dataset]:
+        """Get the raw dataset.
+
+        By default, this method loads the dataset specified in the task's
+        config.jsonnet, and re-split it based on split.json if it exists.
+        If the task creator wishes to mix and match data points, they can
+        override this method.
+
+        Returns:
+            A dictionary containing key-value pairs for the raw datasets.
+            The keys are strings representing the name of the dataset split
+            (e.g., "train", "validation", "test") and the values are
+            HuggingFace `datasets.Dataset` objects containing the raw data for the corresponding split.
+        """
+        ...
