e299caadc69c0440285487a12e3f42496dccd889
Yuval Reif <yuval.reif@simply-air.home>
2023-08-02 11:03:08 +0300

    mnli: fix split names to 'validation', 'test' (instead of 'X_mismatched')

--- src/genbench/tasks/bias_amplified_splits/mnli/task.py
+++ src/genbench/tasks/bias_amplified_splits/mnli/task.py
@@ -1,5 +1,12 @@
+from typing import Any, Callable, Dict, List, Mapping, Optional, Union
+
+from datasets import Dataset, DatasetDict, IterableDataset, IterableDatasetDict, load_dataset
+
 from genbench import Task
-from typing import Any, Dict
+from genbench.api import DatasetSplit
+from genbench.task import resplit_data_source
+from genbench.utils.file import load_jsonnet
+from genbench.utils.tasks import get_task_dir
 
 
 class BiasAmplifiedSplitsMnli(Task):
@@ -27,3 +34,42 @@ class BiasAmplifiedSplitsMnli(Task):
             "input": f"{example['premise']} </s> {example['hypothesis']}",
             "target": example["label"],
         }
+
+    def get_datasets_raw(self) -> Mapping[DatasetSplit, Dataset]:
+        data_source = self._load_data_source()
+
+        # There is a test ("test_split_file") that verifies the split file.
+        # It expects "train", "validation", "test" to be in the split definition,
+        # but in MNLI there are validation_matched/mismatched, etc.
+        # however, to define "validation"/"test" in the splits file, we need these to exist in the original dataset.
+        # so define "validation", "test" sets just so the keys exist in the dataset.
+        data_source["validation"] = data_source["validation_matched"]
+        data_source["test"] = data_source["test_matched"]
+
+        if self.config.split_file is not None:
+            split_file_path = get_task_dir(self.root_task_id, self.subtask_id) / self.config.split_file
+            splitting_info = load_jsonnet(split_file_path)
+            data_source = resplit_data_source(data_source, splitting_info)
+
+        output = {}
+        for split in sorted(data_source.keys()):
+            dataset = data_source[split]
+            output[split] = dataset.map(
+                self.format_example,
+                num_proc=self.dataset_format_num_proc,
+                batched=self.dataset_format_batched,
+                desc=f"Formatting `{split}` examples",
+            )
+            assert all([f in output[split].column_names for f in ["input", "target"]])
+
+        # Assign id to each example
+        for split in sorted(output.keys()):
+            output[split] = output[split].map(
+                lambda example, idx: {"_genbench_idx": idx},
+                with_indices=True,
+                num_proc=self.dataset_format_num_proc,
+                batched=False,
+                desc=f"Assigning id to `{split}` examples",
+            )
+
+        return output
