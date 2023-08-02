4fae897714b6350807cfd7830370e6df250a41ae
Yuval Reif <yuval.reif@simply-air.home>
2023-08-02 10:38:01 +0300

    wanli: update dataset to contain a 'validation' split

--- src/genbench/tasks/bias_amplified_splits/wanli/task.py
+++ src/genbench/tasks/bias_amplified_splits/wanli/task.py
@@ -1,7 +1,9 @@
 from genbench import Task
-from typing import Any, Dict
+from typing import Any, Callable, List, Mapping, Optional, Union, Dict
+from datasets import Dataset, DatasetDict, IterableDataset, IterableDatasetDict
 
 WANLI_LABEL2INT = {'entailment': 0, 'neutral': 1, 'contradiction': 2}
+
 
 class BiasAmplifiedSplitsWanli(Task):
     def format_example(self, example: Dict[str, Any]) -> Dict[str, Any]:
@@ -28,3 +30,26 @@ class BiasAmplifiedSplitsWanli(Task):
             "input": f"{example['premise']} </s> {example['hypothesis']}",
             "target": WANLI_LABEL2INT[example["gold"]],
         }
+
+    def _load_data_source(
+        self,
+    ) -> Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset]:
+        """
+        Private method to load the data source based on the type specified in the configuration.
+
+        The data source can be of two types: 'manual' or 'hf'.
+        For 'manual' type, it loads JSON datasets from the specified test, validation, and train files.
+        For 'hf' type, it loads datasets from the HuggingFace datasets hub using the given HuggingFace dataset ID(s)
+        and the git commit SHA for the specified version of the dataset.
+
+        Returns:
+            Loaded dataset which can be any of the following types:
+            DatasetDict, Dataset, IterableDatasetDict, IterableDataset.
+
+        Raises:
+            ValueError: If the specified data source type is not supported.
+        """
+        data_source = super(BiasAmplifiedSplitsWanli)._load_data_source()
+        data_source['validation'] = data_source['test']
+        return data_source
+
