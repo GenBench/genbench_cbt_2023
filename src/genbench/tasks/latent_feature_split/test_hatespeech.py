4ad8b6e8d4d0b90c049feb197a036f0c12b4c21c
MaikeZuefle <maike@r-zuefle.de>
2023-09-02 09:35:48 +0100

    changed name

---
+++ src/genbench/tasks/latent_feature_split/test_hatespeech.py
@@ -0,0 +1,8 @@
+from genbench import load_task
+from genbench.api import PreparationStrategy
+
+
+task = load_task("latent_feature_split:bert_closest_split")
+ds = task.get_prepared_datasets(PreparationStrategy.FINETUNING)
+print(ds)
+print(ds["test"][0])
